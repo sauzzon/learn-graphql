dfcf68f3300933a6c2eceacc17bf039a43318138
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-06-06 21:46:44 +0545

    Update App.js

--- tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
+++ tutorials/frontend/react-apollo-hooks/app-boilerplate/src/components/App.js
@@ -18,7 +18,7 @@ import {
 import useAccessToken from "../hooks/useAccessToken";
 
 const createApolloClient = (authToken) => {
-  //console.log("auth token found", authToken);
+  console.log("auth token found", authToken);
   return new ApolloClient({
     link: new HttpLink({
       uri: "https://hasura.io/learn/graphql",
